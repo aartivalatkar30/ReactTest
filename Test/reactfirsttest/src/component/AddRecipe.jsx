f8bf0f4db96726fa710c139004db37e564d88fe6
aartivalatkar30 <2020.aarti.valatkar@ves.ac.in>
2024-07-05 10:56:18 +0530

    Update AddRecipe.jsx

--- Test/reactfirsttest/src/component/AddRecipe.jsx
+++ Test/reactfirsttest/src/component/AddRecipe.jsx
@@ -1,43 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const AddRecipe = () => {
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
-      <textarea
-        placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        required
-      />
-      <textarea
-        placeholder="Ingredients"
-        value={ingredients}
-        onChange={(e) => setIngredients(e.target.value)}
-        required
-      />
-      <textarea
-        placeholder="Preparation Steps"
-        value={steps}
-        onChange={(e) => setSteps(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Image URL"
-        value={image}
-        onChange={(e) => setImage(e.target.value)}
-        required
-      />
-      <button type="submit">Add Recipe</button>
-    </form>
-  );
-};
+    <div>
+      <h1>Add Recipe</h1>
+    </div>
+  )
+}
 
-export default AddRecipe;
+export default AddRecipe
